ec9f82d2dab5988e5816825006d77d896999b3e0
Peter Beckwith <beckwithp16@gmail.com>
2025-03-06 20:29:44 -0500

    Add files via upload
    Added Save button and new text box
    
    Known issues: 
    - Text does not save to excel file, 
    - How to put text in different columns and cells

--- ticketing-system.py
+++ ticketing-system.py
@@ -1,23 +1,29 @@
 import tkinter as tk
 from selectors import SelectSelector
 from tkinter import * #import everything from tkinter (i think?)
+from tkinter import filedialog
 
-root = tk.Tk()
+window = tk.Tk()
+
+def saveFile():
+    filedialog.asksaveasfilename(initialdir='D:\Boynton Beach Tech\Ticketing System', )
 
 def click(): # function which will be used on the button
     print('File has been saved!')
+
 x = IntVar()
+
 def display():
     if x.get()==1:
         print('This is a returning customer.')
     else:
         print('This is not a returning customer.')
 
-root.title='my first GUI' # title of the window
-root.geometry='100x100' # determines the size of the window
-root.config(bg ='black') # makes the background of the window black
+window.title='my first GUI' # title of the window
+window.geometry='100x100' # determines the size of the window
+window.config(bg ='black') # makes the background of the window black
 
-label=tk.Label(root, text='Customer Name:', # creates a label
+label=tk.Label(window, text='Customer Name:', # creates a label
                font=('Arial', 20),
                fg='white',
                relief=RAISED,
@@ -27,7 +33,7 @@ label=tk.Label(root, text='Customer Name:', # creates a label
                bg='black',)
 label.pack() # .pack puts the widget onto the window
 
-entry = tk.Text(root) # user input widget
+entry = tk.Text(window) # user input widget
 entry.config(bg='#252525',  # makes text white and background black
             fg='white',
             font=('Arial', 14),
@@ -36,15 +42,20 @@ entry.config(bg='#252525',  # makes text white and background black
             relief=RAISED,)
 entry.pack() # puts widget onto window
 
-checkbox = Checkbutton(root,text='Is this a Returning Customer?', variable=x, onvalue=1, offvalue=0, command=display)
+checkbox = Checkbutton(window,text='Is this a Returning Customer?', variable=x, onvalue=1, offvalue=0, command=display)
 (checkbox.config(font=('Arial', 20),
                  fg='white',
                  bg='black',
                  activeforeground='black',))
 checkbox.pack()
 
+button = Button(text='Save', command=saveFile) # saves file
+button.pack()
 
-button=tk.Button(root, text='Save File!') # Creates a button that says "Save File"
+text = Text(window)
+text.pack()
+
+button=tk.Button(window, text='Save File!') # Creates a button that says "Save File"
 button.config( #
     command=click,# calls back function 'click'
     font=('Arial', 20), # font and size
@@ -52,4 +63,6 @@ button.config( #
     fg='white') # foreground color
 button.pack() # displays button on window
 
-root.mainloop() # continually displays window
+
+
+window.mainloop() # continually displays window
